7bb044637dd1a9e291820b4756e62caffe540a8d
NatalieShamir <nataliashamir85@gmail.com>
2022-05-03 12:26:22 +0300

    add form for adding a card

--- scripts/index.js
+++ scripts/index.js
@@ -27,15 +27,17 @@ const initialCards = [
 ];
 
 //Modals
-const addCardModal = document.querySelector(".popup_type_add-card");
-const editProfileModal = document.querySelector(".popup_type_edit-profile");
+const addCard = document.querySelector(".popup_type_add-card");
+const editProfile = document.querySelector(".popup_type_edit-profile");
 const form = document.querySelector(".popup__form");
 const inputName = document.querySelector(".popup__form-input_type_name");
 const inputJob = document.querySelector(".popup__form-input_type_job");
 
 //Buttons and Other DOM Elements
+const openEditProfileButton = document.querySelector(".profile__edit-button");
 const closeEditProfileButton = document.querySelector(".popup_close_profile");
-const openEditProfileButton = document.querySelector(".popup_close_add-card");
+const openAddCardButton = document.querySelector(".profile__add-button");
+const closeAddCardButton = document.querySelector(".popup_close_add-card");
 const profileName = document.querySelector(".profile__title");
 const profileJob = document.querySelector(".profile__subtitle");
 
@@ -62,8 +64,8 @@ function renderCard(card, wrapper) {
   wrapper.append(createCardElement(card));
 }
 
-function openPopup() {
-  popup.classList.add("popup_open");
+function openEditProfilePopup() {
+  editProfile.classList.add("popup_open");
 
   const profileNameText = profileName.textContent;
   const profileJobText = profileJob.textContent;
@@ -81,16 +83,29 @@ function onSubmit(event) {
   profileName.textContent = inputNameValue;
   profileJob.textContent = inputJobValue;
 
-  closePopup();
+  closeEditProfilePopup();
 }
 
-function closePopup() {
-  popup.classList.remove("popup_open");
+function closeEditProfilePopup() {
+  editProfile.classList.remove("popup_open");
+}
+
+function openAddCardModal() {
+  addCard.classList.add("popup_open");
+}
+
+function onSubmit(event) {
+  closeAddCardPopup();
+}
+
+function closeAddCardPopup() {
+  addCard.classList.remove("popup_open");
 }
 
 //Event Handlers
-openEditProfileButton.addEventListener("click", openPopup);
+openEditProfileButton.addEventListener("click", openEditProfilePopup);
+openAddCardButton.addEventListener("click", openAddCardModal);
+closeAddCardButton.addEventListener("click", closeAddCardPopup);
 form.addEventListener("submit", onSubmit);
-closeEditProfileButton.addEventListener("click", closePopup);
-
+closeEditProfileButton.addEventListener("click", closeEditProfilePopup);
 initialCards.forEach((card) => renderCard(card, cardsGallery));
