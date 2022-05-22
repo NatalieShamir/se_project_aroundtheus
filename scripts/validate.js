f596d1a5366c15fe709afdfdb533eec15301f3d4
NatalieShamir <nataliashamir85@gmail.com>
2022-05-22 11:12:41 +0300

    find all forms on page

---
+++ scripts/validate.js
@@ -0,0 +1,14 @@
+function enableValidation(settings) {
+  const forms = Array.from(document.querySelectorAll(".popup__form"));
+}
+
+const config = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+enableValidation(config);
