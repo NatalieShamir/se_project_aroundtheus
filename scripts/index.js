3eccd43ab0db8ca94e7e84fbc70cfd38880d0190
NatalieShamir <nataliashamir85@gmail.com>
2022-06-09 10:46:37 +0300

    refactor: change keyHandler function name to handleKeyDown

--- scripts/index.js
+++ scripts/index.js
@@ -73,13 +73,13 @@ const popupPreviewCaption = document.querySelector(".popup__preview-caption");
 function openPopup(popup) {
   popup.classList.add("popup_open");
   popup.addEventListener("mousedown", closePopupOnRemoteClick);
-  document.addEventListener("keydown", keyHandler);
+  document.addEventListener("keydown", handleKeyDown);
 }
 
 function closePopup(popup) {
   popup.classList.remove("popup_open");
   popup.removeEventListener("mousedown", closePopupOnRemoteClick);
-  document.removeEventListener("keydown", keyHandler);
+  document.removeEventListener("keydown", handleKeyDown);
 }
 
 function fillProfileForm() {
@@ -154,7 +154,7 @@ function closePopupOnRemoteClick(evt) {
   }
 }
 
-function keyHandler(evt) {
+function handleKeyDown(evt) {
   if (evt.key === "Escape") {
     const openedPopup = document.querySelector(".popup_open");
     closePopup(openedPopup);
