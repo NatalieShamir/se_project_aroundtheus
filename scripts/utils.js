1dd078405a6ad5bdbab9c7c8e63d4e9a897692af
NatalieShamir <nataliashamir85@gmail.com>
2022-06-19 11:06:16 +0300

    create utils.js and add handlers and the function that opens/closes modal windows to it

---
+++ scripts/utils.js
@@ -0,0 +1,26 @@
+export const popupPreviewImage = document.querySelector(
+  ".popup__preview-image"
+);
+export const popupPreviewCaption = document.querySelector(
+  ".popup__preview-caption"
+);
+export const addPreviewPopup = document.querySelector(".popup_type_preview");
+
+export function openPopup(popup) {
+  popup.classList.add("popup_open");
+  popup.addEventListener("mousedown", closePopupOnRemoteClick);
+  document.addEventListener("keydown", handleKeyDown);
+}
+
+function closePopupOnRemoteClick(evt) {
+  if (evt.target === evt.currentTarget) {
+    closePopup(evt.target);
+  }
+}
+
+function handleKeyDown(evt) {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector(".popup_open");
+    closePopup(openedPopup);
+  }
+}
