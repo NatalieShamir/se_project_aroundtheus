325d10262d10185b36f8c7b42927947cb6e972c6
NatalieShamir <nataliashamir85@gmail.com>
2022-07-07 12:19:57 +0300

    add components folder to project directory and move Card.js and FormValidator.js to it

---
+++ components/Card.js
@@ -0,0 +1,63 @@
+import {
+  openPopup,
+  addPreviewPopup,
+  popupPreviewImage,
+  popupPreviewCaption,
+} from "./utils.js";
+//Creates a Card with Text and an Image Link
+
+export class Card {
+  constructor({ name, link }, templateCardSelector) {
+    this._name = name;
+    this._link = link;
+    this._cardTemplate = document
+      .querySelector(templateCardSelector)
+      .content.querySelector(".card");
+  }
+
+  //Private Method for Adding Event Listeners
+
+  _setEventListeners() {
+    const likeCardButton =
+      this._cardElement.querySelector(".card__like-button");
+    const deleteCardButton = this._cardElement.querySelector(
+      ".card__delete-button"
+    );
+    const cardImage = this._cardElement.querySelector(".card__image");
+    likeCardButton.addEventListener("click", this._handleLikeIcon);
+    deleteCardButton.addEventListener("click", this._handleDeleteCard);
+    cardImage.addEventListener("click", () => this._handlePreviewImage());
+  }
+
+  //Private Methods for Each One of the Handlers
+
+  _handleLikeIcon = (evt) =>
+    evt.target.classList.toggle("card__like-button_active");
+
+  _handleDeleteCard = () => {
+    this._cardElement.remove();
+    this._cardElement = null;
+  };
+
+  _handlePreviewImage = () => {
+    openPopup(addPreviewPopup);
+    popupPreviewImage.src = this._link;
+    popupPreviewImage.alt = `Photo of ${this._name}`;
+    popupPreviewCaption.textContent = this._name;
+  };
+
+  //Returns a Fully Functional Card Element Populated with Data
+
+  getCardElement = () => {
+    this._cardElement = this._cardTemplate.cloneNode(true);
+
+    const cardTitle = this._cardElement.querySelector(".card__title");
+    const cardImage = this._cardElement.querySelector(".card__image");
+
+    cardTitle.textContent = this._name;
+    cardImage.style.backgroundImage = `url(${this._link})`;
+
+    this._setEventListeners();
+    return this._cardElement;
+  };
+}
---
+++ components/FormValidator.js
@@ -0,0 +1,80 @@
+class FormValidator {
+  constructor(settings, formElement) {
+    this._settings = settings;
+    this._formElement = formElement;
+  }
+
+  _showInputError(inputElement, errorMessage) {
+    const { inputErrorClass, errorClass } = this._settings;
+    const errorElement = this._formElement.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.add(inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(errorClass);
+  }
+
+  _hideInputError(inputElement) {
+    const { inputErrorClass, errorClass } = this._settings;
+    const errorElement = this._formElement.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.remove(inputErrorClass);
+    errorElement.textContent = "";
+    errorElement.classList.remove(errorClass);
+  }
+
+  _checkInputValidity(inputElement) {
+    if (inputElement.validity.valid) {
+      this._hideInputError(inputElement);
+    } else {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    }
+  }
+
+  _setEventListeners() {
+    const { inputSelector } = this._settings;
+    this.inputElements = [...this._formElement.querySelectorAll(inputSelector)];
+    this.inputElements.forEach((inputElement) => {
+      inputElement.addEventListener("input", (e) => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  _isAllInputsValid() {
+    return this.inputElements.every(
+      (inputElement) => inputElement.validity.valid === true
+    );
+  }
+
+  _toggleButtonState() {
+    const { inactiveButtonClass, submitButtonSelector } = this._settings;
+    const buttonElement = this._formElement.querySelector(submitButtonSelector);
+
+    if (this._isAllInputsValid()) {
+      buttonElement.disabled = false;
+      buttonElement.classList.remove(inactiveButtonClass);
+    } else {
+      buttonElement.disabled = true;
+      buttonElement.classList.add(inactiveButtonClass);
+    }
+  }
+
+  resetValidation() {
+    this.inputElements.forEach((inputElement) => {
+      this._hideInputError(inputElement);
+    });
+
+    this._toggleButtonState();
+  }
+
+  enableValidation() {
+    this._formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
+}
+export default FormValidator;
