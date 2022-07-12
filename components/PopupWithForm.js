bede22b8b36f310bb123fcb020919a21ba3f587e
NatalieShamir <nataliashamir85@gmail.com>
2022-07-12 13:19:05 +0300

    add _getInputValues() to PopupWithForm class

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,8 +1,32 @@
-import { Popup } from "./Popup";
+import { Popup } from "./Popup.js";
 
 export class PopupWithForm extends Popup {
   constructor(popupSelector, handleSubmit) {
     super(popupSelector);
     this._handleSubmit = handleSubmit;
+    this._formElement = this._popupElement.querySelector(".popup__form");
+  }
+
+  _getInputValues() {
+    const values = {};
+
+    const inputs = [
+      ...this._formElement.querySelectorAll(".popup__form-input"),
+    ];
+
+    inputs.forEach((input) => {
+      const key = input.name;
+      const value = input.value;
+
+      values[key] = value;
+    });
+
+    return values;
   }
 }
+// /*
+//   setEventListeners() {
+//     super.setEventListeners(); //super allows to save the parent class functionality and to extend it to additional functionality
+//   }
+//
+//  */
