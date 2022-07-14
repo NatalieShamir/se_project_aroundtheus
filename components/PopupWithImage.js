33d8bcf5e551afaac43ecc9170c22ca39be7de89
NatalieShamir <nataliashamir85@gmail.com>
2022-07-14 15:00:42 +0300

    create PopupWithImage class featuring modified open() which features the functionality for adding an image to the popup and the corresponding image src attribute along with a caption for the image

---
+++ components/PopupWithImage.js
@@ -0,0 +1,16 @@
+import { Popup } from "./Popup.js";
+
+export class PopupWithImage extends Popup {
+  // constructor(popupSelector) {
+  //     super(popupSelector)
+  // }
+  //
+  open(link, name) {
+    super.open();
+    const image = this._popupElement.querySelector(".popup__preview-image");
+    const caption = this._popupElement.querySelector(".popup__preview-caption");
+
+    image.src = link;
+    caption.textContent = name;
+  }
+}
