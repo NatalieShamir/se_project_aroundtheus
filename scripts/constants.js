a98131e3de582704dbd1d9a7e71ab0e61b0e6343
NatalieShamir <nataliashamir85@gmail.com>
2022-07-18 11:50:32 +0300

    move all constants from index.js to constants.js and add export and import statements in accordance with

---
+++ scripts/constants.js
@@ -0,0 +1,69 @@
+export const settings = {
+  inputSelector: ".popup__form-input",
+  submitButtonSelector: ".popup__form-button",
+  inactiveButtonClass: "popup__form-button_disabled",
+  inputErrorClass: "popup__form-input_type_error",
+  errorClass: "popup__form-error_visible",
+};
+
+//Modals
+export const editProfilePopup = document.querySelector(
+  ".popup_type_edit-profile"
+);
+export const addCardPopup = document.querySelector(".popup_type_add-card");
+
+//Forms
+export const editProfileForm = editProfilePopup.querySelector(
+  ".popup__form_type_edit"
+);
+export const openAddCardButton = document.querySelector(".profile__add-button");
+export const addCardForm = addCardPopup.querySelector(
+  ".popup__form_type_add-card"
+);
+
+//Declarations
+export const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+//Buttons and Other DOM Elements
+export const openEditProfileButton = document.querySelector(
+  ".profile__edit-button"
+);
+
+//Inputs
+export const inputNameValue = document.querySelector(
+  ".popup__form-input_type_name"
+);
+export const inputJobValue = document.querySelector(
+  ".popup__form-input_type_job"
+);
+
+//Templates
+export const cardTemplateSelector = "#card-template";
+
+//Wrappers
+export const cardsGallery = document.querySelector(".cards__gallery");
