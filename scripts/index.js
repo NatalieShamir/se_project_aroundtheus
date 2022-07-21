c7af8c633e19ac38886258e33eebb7d32c66a71d
NatalieShamir <nataliashamir85@gmail.com>
2022-07-21 15:21:19 +0300

    remove unnecesary utils.js file

--- scripts/index.js
+++ scripts/index.js
@@ -4,66 +4,18 @@ import { PopupWithForm } from "../components/PopupWithForm.js";
 import { PopupWithImage } from "../components/PopupWithImage.js";
 import { Section } from "../components/Section.js";
 import { UserInfo } from "../components/UserInfo.js";
-
-const settings = {
-  inputSelector: ".popup__form-input",
-  submitButtonSelector: ".popup__form-button",
-  inactiveButtonClass: "popup__form-button_disabled",
-  inputErrorClass: "popup__form-input_type_error",
-  errorClass: "popup__form-error_visible",
-};
-
-//Modals
-const editProfilePopup = document.querySelector(".popup_type_edit-profile");
-const addCardPopup = document.querySelector(".popup_type_add-card");
-
-//Forms
-const editProfileForm = editProfilePopup.querySelector(
-  ".popup__form_type_edit"
-);
-const openAddCardButton = document.querySelector(".profile__add-button");
-const addCardForm = addCardPopup.querySelector(".popup__form_type_add-card");
-
-//Declarations
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
-  },
-];
-
-//Buttons and Other DOM Elements
-const openEditProfileButton = document.querySelector(".profile__edit-button");
-
-//Inputs
-const inputNameValue = document.querySelector(".popup__form-input_type_name");
-const inputJobValue = document.querySelector(".popup__form-input_type_job");
-
-//Templates
-const cardTemplateSelector = "#card-template";
-
-//Wrappers
-const cardsGallery = document.querySelector(".cards__gallery");
+import {
+  settings,
+  editProfileForm,
+  openAddCardButton,
+  addCardForm,
+  initialCards,
+  openEditProfileButton,
+  inputNameValue,
+  inputJobValue,
+  cardTemplateSelector,
+  cardsGallery,
+} from "./constants.js";
 
 //Form Validator Instances
 const editProfileFormValidator = new FormValidator(settings, editProfileForm);
