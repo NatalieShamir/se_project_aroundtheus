ffbb6ace0ee36de3e0c78339d11927235557aa95
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-08-12 09:17:50 +0300

    change the rule for the MiniCssExtractPlugin loader

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
+            options: { importLoaders: 1 },
           },
           "postcss-loader",
         ],
