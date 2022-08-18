202bf3ad04ffcf78ade818d404e5507a53542a56
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-08-18 23:44:09 +0300

    fix: correct path url path

--- utils/Api.js
+++ utils/Api.js
@@ -1,11 +1,11 @@
 class Api {
   constructor(options) {
-    this._baseUrl = options._baseUrl;
-    this._headers = options._headers;
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
   }
 
   getUserInfo() {
-    return fetch(this._baseUrl + "users/me/", {
+    return fetch(this._baseUrl + "/users/me/", {
       headers: this._headers,
     }).then((res) => (res.ok ? res.json() : Promise.reject(res.statusText)));
   }
@@ -17,7 +17,7 @@ class Api {
   }
 
   editProfile(name, about) {
-    return (fetch(this._baseUrl + "users/me"),
+    return (fetch(this._baseUrl + "/users/me"),
     {
       method: "PATCH",
       headers: this._headers,
@@ -44,7 +44,7 @@ class Api {
 export const api = new Api({
   baseUrl: "https://around.nomoreparties.co/v1/cohort-3-en",
   headers: {
-    authorization: " 493dfbd5-914b-4624-a274-ddbc55b172ae",
+    authorization: "c38a1131-295f-471c-b53b-3cfda4699ea7",
     "Content-Type": "application/json",
   },
 });
