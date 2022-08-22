8ff5801e77e3b66565879344a7abb28c68e78908
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-08-22 11:37:40 +0300

    create getId()

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,36 +13,8 @@ export class Card {
     this._handleLikeIcon = handleLikeIcon;
   }
 
-  //Private Method for Adding Event Listeners
-
-  _setEventListeners() {
-    const likeCardButton =
-      this._cardElement.querySelector(".card__like-button");
-    const deleteCardButton = this._cardElement.querySelector(
-      ".card__delete-button"
-    );
-    const cardImage = this._cardElement.querySelector(".card__image");
-    likeCardButton.addEventListener("click", this._handleLikeIcon);
-    deleteCardButton.addEventListener("click", this._handleDeleteCard);
-    cardImage.addEventListener("click", () =>
-      this._handleCardClick(this._link, this._name)
-    );
-  }
-
-  //Private Methods for Each One of the Handlers
-  /* 
-  _handleLikeIcon = (evt) =>
-    evt.target.classList.toggle("card__like-button_active");
- */
-  _handleDeleteCard = () => {
-    this._cardElement.remove();
-    this._cardElement = null;
-  };
-
-  _setLikes = () => {
-    const likesAmount = this._likes.length;
-    this._cardElement.querySelector(".card__likes-amount").textContent =
-      likesAmount;
+  getId = () => {
+    return this._id;
   };
 
   //Returns a Fully Functional Card Element Populated with Data
@@ -61,5 +33,37 @@ export class Card {
     this._setLikes();
 
     return this._cardElement;
+  };
+
+  //Private Method for Adding Event Listeners
+
+  _setEventListeners() {
+    const likeCardButton =
+      this._cardElement.querySelector(".card__like-button");
+    const deleteCardButton = this._cardElement.querySelector(
+      ".card__delete-button"
+    );
+    const cardImage = this._cardElement.querySelector(".card__image");
+    likeCardButton.addEventListener("click", this._handleLikeIcon);
+    deleteCardButton.addEventListener("click", this._handleDeleteCard);
+    cardImage.addEventListener("click", () =>
+      this._handleCardClick(this._link, this._name)
+    );
+  }
+
+  _setLikes = () => {
+    const likesAmount = this._likes.length;
+    this._cardElement.querySelector(".card__likes-amount").textContent =
+      likesAmount;
+  };
+
+  //Private Methods for Each One of the Handlers
+  /* 
+  _handleLikeIcon = (evt) =>
+    evt.target.classList.toggle("card__like-button_active");
+ */
+  _handleDeleteCard = () => {
+    this._cardElement.remove();
+    this._cardElement = null;
   };
 }
