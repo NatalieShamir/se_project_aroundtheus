a52d075e6f8d6af780df2d363feb399e37417b41
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-08-22 15:52:09 +0300

    fix: syntax of this._id

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ export class Card {
     this._name = data.name;
     this._link = data.link;
     this._likes = data.likes;
-    this._id = data.id;
+    this._id = data._id;
     this._cardTemplate = document
       .querySelector(templateCardSelector)
       .content.querySelector(".card");
