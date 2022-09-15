01af84ebfa15cb31a69084c98a5528fdab8dc283
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2022-09-15 09:21:41 +0300

    refactor: add constructor with image and caption in its body

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,13 +1,16 @@
 import { Popup } from "./Popup.js";
 
 export class PopupWithImage extends Popup {
+  constructor(popupSelector, handleSubmit) {
+    super(popupSelector);
+    this._image = this._popupElement.querySelector(".popup__preview-image");
+    this._caption = this._popupElement.querySelector(".popup__preview-caption");
+  }
   open(link, name) {
     super.open();
-    const image = this._popupElement.querySelector(".popup__preview-image");
-    const caption = this._popupElement.querySelector(".popup__preview-caption");
 
-    image.src = link;
-    image.alt = "Clicked-on image enlarged";
-    caption.textContent = name;
+    this._image.src = link;
+    this._image.alt = "Clicked-on image enlarged";
+    this._caption.textContent = name;
   }
 }
